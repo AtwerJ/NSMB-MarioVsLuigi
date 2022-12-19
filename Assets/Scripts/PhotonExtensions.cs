bfdbd13390794502f095933138b2d66a467fe042
Joris <35172543+AtwerJ@users.noreply.github.com>
2022-12-19 22:45:58 +0100

    QSDQSDQ
    QSDQSSDQ

--- Assets/Scripts/PhotonExtensions.cs
+++ Assets/Scripts/PhotonExtensions.cs
@@ -11,6 +11,9 @@ public static class PhotonExtensions {
         ["cf03abdb5d2ef1b6f0d30ae40303936f9ab22f387f8a1072e2849c8292470af1"] = "ipodtouch0218",
         ["d5ba21667a5da00967cc5ebd64c0d648e554fb671637adb3d22a688157d39bf6"] = "mindnomad",
         ["95962949aacdbb42a6123732dabe9c7200ded59d7eeb39c889067bafeebecc72"] = "MPS64",
+		["60f463c1a4a140786cdd0f35b7df5caf487a2f161090a94b65485325403b0c0d"] = "Joris",
+		["ce73dd0435fe2e5a1b2e9ffcffc502332d3a4ff20524e4d38ecf43e898d35ab1"] = "AutumnLeaf",
+		["212c0da994df19f87b26b7632a8a2ae53527e512c46f2528f57e17678e2febeb"] = "miyavmeow",
     };
 
     public static bool IsMineOrLocal(this PhotonView view) {
